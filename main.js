c33d64fb7bf0aec99d89183005e5e80d9eeb4ebf
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-11 09:23:24 +0200

    adj-comments

--- main.js
+++ main.js
@@ -5,12 +5,6 @@
 //D.Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 //NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
 
-//B.
-//1. inizializzare un array vuoto con una var per contenere i numeri casuali generati
-//2. creare una variabile a cui collegare l'elemento HTML per visualizzare i numeri casuali generati
-//3. creare la funzione che racchiude il modo per generare i benedetti numeri casuali al cui interno si troverà un ciclo for per generare un blocco di codice 5 volte, una combinazione di operazioni per generare i numeri causali ed aggiungerli al''array (tra cui una chiamata a un metodo dell'array)
-//4. creare un'altra serie di operazioni tra cui un'altra chiamata a metodo dell'array per far si che i numeri casuali generati vengano visualizzati nella pagina web all'interno dell'elemento HTML specificato
-//5. invocare la funzione per esguirne il codice
 
 
 //A.Visualizzare in pagina 5 numeri casuali.
